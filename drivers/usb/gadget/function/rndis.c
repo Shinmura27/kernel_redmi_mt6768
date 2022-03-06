f8e14f6d385cb5ca54453e29480e4e0a1bf504a2
Ariel Syafiqri <88129448+CincauEXE@users.noreply.github.com>
2022-03-06 14:26:40 +0700

    Fix error: use of undeclared identifier 'n'

--- drivers/usb/gadget/function/rndis.c
+++ drivers/usb/gadget/function/rndis.c
@@ -985,7 +985,8 @@ struct rndis_params *rndis_register(void (*resp_avail)(void *v), void *v)
 	params->media_state = RNDIS_MEDIA_STATE_DISCONNECTED;
 	params->resp_avail = resp_avail;
 	params->v = v;
-	INIT_LIST_HEAD(&params->resp_queue);
+	params->max_pkt_per_xfer = 1;
+	INIT_LIST_HEAD(&(params->resp_queue));
 	spin_lock_init(&params->resp_lock);
 	pr_debug("%s: configNr = %d\n", __func__, i);
 
@@ -1090,9 +1091,16 @@ void rndis_free_response(struct rndis_params *params, u8 *buf)
 	rndis_resp_t *r;
 	struct list_head *act, *tmp;
 
+	if (rndis_debug > 2)
+		RNDIS_DBG("\n");
+
 	spin_lock(&params->resp_lock);
-	list_for_each_entry_safe(r, n, &params->resp_queue, list) {
-		if (r->buf == buf) {
+	list_for_each_safe(act, tmp, &(params->resp_queue)) {
+		if (!act)
+			continue;
+
+		r = list_entry(act, rndis_resp_t, list);
+		if (r && r->buf == buf) {
 			list_del(&r->list);
 			kfree(r);
 		}
@@ -1109,7 +1117,8 @@ u8 *rndis_get_next_response(struct rndis_params *params, u32 *length)
 	if (!length) return NULL;
 
 	spin_lock(&params->resp_lock);
-	list_for_each_entry_safe(r, n, &params->resp_queue, list) {
+	list_for_each_safe(act, tmp, &(params->resp_queue)) {
+		r = list_entry(act, rndis_resp_t, list);
 		if (!r->send) {
 			r->send = 1;
 			*length = r->length;
@@ -1139,7 +1148,7 @@ static rndis_resp_t *rndis_add_response(struct rndis_params *params, u32 length)
 	r->send = 0;
 
 	spin_lock(&params->resp_lock);
-	list_add_tail(&r->list, &params->resp_queue);
+	list_add_tail(&r->list, &(params->resp_queue));
 	spin_unlock(&params->resp_lock);
 	return r;
 }
